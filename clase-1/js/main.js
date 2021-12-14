5dd04d3b1409f792b6b8a2d8dd5f771d88af6f45
lucianoblanco <lucianoblanco1994@gmail.com>
2021-12-14 12:07:16 -0300

    Agregamos carpeta de la clase 1

---
+++ clase-1/js/main.js
@@ -0,0 +1,70 @@
+console.log("Hola mundo #2");
+
+// Comentario de una línea
+
+/* Comentario de más de una línea */
+
+var nombreVariable1 = "Luciano";
+nombreVariable1 = "Programación";
+let nombreVariable2 = "Curso de Javascript";
+const nombreVariable3 = "Coderhouse";
+const pi = 3.14;
+const iva21 = 1.21;
+// La constantes no cambian en el programa
+
+console.log(nombreVariable1);
+nombreVariable1 = "Curso de programación";
+console.log(nombreVariable2);
+console.log(nombreVariable3);
+console.log(iva21);
+console.log(iva21 + pi);
+
+// Variables númericas
+let a = 1;
+let b = 2;
+let c = 3;
+
+//Suma de  variables
+let suma = a + b + c;
+console.log(suma);
+
+// Resta de variables
+let resta = a - c;
+console.log(resta);
+
+// Multiplico variables
+let multi = b * c;
+console.log(multi);
+
+// Avanzada
+let avanzada = a + (b*c);
+console.log(avanzada);
+
+// Operaciones con Textos
+
+let textoA = "Coder";
+let textoB = "House";
+const blanco = " ";
+
+let resultado1 = textoA + textoB;
+console.log(resultado1);
+let resultado2 = textoA + blanco + textoB;
+console.log(resultado2);
+let resultado3 = textoB + " " + textoA;
+console.log(resultado3);
+console.log("Estoy estudiando Javascript en " + resultado1);
+
+// Prompt, console, y alerts
+
+let nombre = prompt("Ingrese su nombre: ");
+let apellido = prompt("Apellido: ");
+let edad = prompt("Edad: ");
+let mensaje_final = "Mi nombre es: " + nombre + " " + apellido + ". Tengo " + edad + " años.";
+console.log( mensaje_final);
+alert(mensaje_final);
+
+
+
+
+
+ 
